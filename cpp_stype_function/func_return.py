8ef1bda7e1ecb343baaed2f03f41e3a82caa5264
Anglersking <418848424@qq.com>
2024-03-25 11:16:48 +0800

    par function type

---
+++ cpp_stype_function/func_return.py
@@ -0,0 +1,8 @@
+# from typing import Dict, List  
+
+def function_A(par:dict[str,int]) -> dict[str,int]: #None
+    print("213",par)
+    return True
+print(function_A({'1':'1'}))
+el: list[str]=['2',4]
+print(el)
